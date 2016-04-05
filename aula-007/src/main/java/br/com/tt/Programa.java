38fb2e92f13b171ed13a9d2f3295ae77b650a5ae
sala03 <sala03@targettrust.local>
2016-04-05 19:04:42 -0300

    Segunda Aula

---
+++ aula-007/src/main/java/br/com/tt/Programa.java
@@ -0,0 +1,14 @@
+package br.com.tt;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Operadores");
+		
+		Long soma = 1L + 5L;
+		System.out.println(soma);
+
+	}
+
+}
