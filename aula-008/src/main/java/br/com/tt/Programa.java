2183b6f49ab610c16e49e7de160acc2e5018e5b9
sala03 <sala03@targettrust.local>
2016-04-05 20:50:24 -0300

    Segunda Aula

--- aula-008/src/main/java/br/com/tt/Programa.java
+++ aula-008/src/main/java/br/com/tt/Programa.java
@@ -8,19 +8,21 @@ public class Programa {
 		Animal cachorro = new Animal();
 		Animal gato = new Animal();
 		cachorro.setApelido("Rex");
-		gato.setApelido("ab");
+		
+		gato.setApelido("abc");
+		gato.setDono(new Pessoa());
+		gato.getDono().setNome("Maria da Silva");
+		gato.getDono().setCpf("0327882469-11");
 		
 		Pessoa donoDoRex = new Pessoa();
 		donoDoRex.setNome("Pedro da Silva");
 		donoDoRex.setCpf("003792469-11");
 		
-		
+				
 		cachorro.setDono(donoDoRex);
-		
-		
-		
+						
 		System.out.println(cachorro.getApelido() + " -- Dono: " + cachorro.getDono().getNome());
-		System.out.println(gato.getApelido());
+		System.out.println(gato.getApelido() + " -- Dono: " + gato.getDono().getNome());
 		
 
 	}
