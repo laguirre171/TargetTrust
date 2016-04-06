d2ec517b9fdeec9b567315b9de97c33a9d9a88e3
sala03 <sala03@targettrust.local>
2016-04-05 21:02:00 -0300

    Segunda Aula

---
+++ aula-009/src/main/java/br/com/tt/Programa.java
@@ -0,0 +1,13 @@
+package br.com.tt;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Double pi = Math.PI;
+		System.out.println(pi);
+		
+
+	}
+
+}
