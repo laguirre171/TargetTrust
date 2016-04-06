1356e1bf3381afa262cea82c1b743797683713c0
sala03 <sala03@targettrust.local>
2016-04-05 21:55:35 -0300

    Segunda Aula

--- aula-010/src/main/java/br/com/tt/TesteSwitch.java
+++ aula-010/src/main/java/br/com/tt/TesteSwitch.java
@@ -1,6 +1,6 @@
 package br.com.tt;
 
-public class Programa {
+public class TesteSwitch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ aula-010/src/main/java/br/com/tt/TesteWhile.java
@@ -0,0 +1,15 @@
+package br.com.tt;
+
+public class TesteWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		while (true) {
+			System.out.println("Teste while");
+			
+		}
+
+	}
+
+}
