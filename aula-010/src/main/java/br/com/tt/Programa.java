1215ced172283a76e0e31f0f1077d12bd00d7d7e
sala03 <sala03@targettrust.local>
2016-04-05 21:49:09 -0300

    Segunda Aula

--- aula-010/src/main/java/br/com/tt/Programa.java
+++ aula-010/src/main/java/br/com/tt/Programa.java
@@ -4,6 +4,24 @@ public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String turno = "M";
+
+		switch (turno) {
+		case "M":
+			System.out.println("Bom dia");
+
+			break;
+		case "T":
+			System.out.println("Boa tarde");
+			break;
+		case "N":
+			System.out.println("Boa Noite");
+			break;
+
+		default:
+			System.out.println("Olá");
+			break;
+		}
 
 	}
 
