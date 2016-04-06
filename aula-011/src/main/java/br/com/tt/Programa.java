2a363f06055c048c39f14b542aa6e30a29c97c69
sala03 <sala03@targettrust.local>
2016-04-06 19:01:26 -0300

    Terceira aula

---
+++ aula-011/src/main/java/br/com/tt/Programa.java
@@ -0,0 +1,36 @@
+package br.com.tt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Programa {
+	List<String> nomes = new ArrayList<>();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 * Funcionalidade utilizada para pesquisar através do nome
+	 * 
+	 * @param nome
+	 * 
+	 * @return
+	 * 
+	 */
+	public static String pesquisar(String nome) { //Pertence a classe
+		return null;
+
+	}
+	
+	static {
+		nomes.add("Pedro");
+		nomes.add("Maria");
+		nomes.add("Carlos");
+		nomes.add("Bruno");
+		nomes.add("Marcia");
+		
+	}
+
+}
