b3447474b9e30f7639224f3a48783dca0f5cfe6a
sala03 <sala03@targettrust.local>
2016-04-06 22:05:17 -0300

    Terceira aula

---
+++ aula-015/src/main/java/br/com/tt/Programa.java
@@ -0,0 +1,14 @@
+package br.com.tt;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		final int i = 0;
+		i = 1; //depois que recebe um valor não muda mais 
+		
+		
+
+	}
+
+}
