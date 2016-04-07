93ac64ab5f7546536bc29100d66b220d90830828
sala03 <sala03@targettrust.local>
2016-04-07 18:58:10 -0300

    Quarta Aula

---
+++ aula-017/src/main/java/br/com/tt/string/String01.java
@@ -0,0 +1,13 @@
+package br.com.tt.string;
+
+public class String01 {
+	
+	public static void main(String[] args) {
+		String nome = "Aguirre";
+		
+		nome.toUpperCase();
+		System.out.println();
+		
+	}
+
+}
