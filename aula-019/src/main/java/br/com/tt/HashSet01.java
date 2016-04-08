b500134d14e78ddec283329a4a1ba0a944449de8
sala03 <sala03@targettrust.local>
2016-04-07 22:36:27 -0300

    Quarta Aula

--- aula-019/src/main/java/br/com/tt/HashSet01.java
+++ aula-019/src/main/java/br/com/tt/HashSet01.java
@@ -1,21 +1,49 @@
 package br.com.tt;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class HashSet01 {
 
+	//não permite duplicação de objetos
 	public static void main(String[] args) {
 		Set<String> frutas = new HashSet<>();
 		frutas.add("maça");
 		frutas.add("maça");
-		frutas.add("maça");
+		frutas.add("banana");
 		frutas.add("maça");
 		
 		for (String fruta : frutas) {
 			System.out.println(fruta);
 			
 		}
+		
+		Set<String> frutas01 = new TreeSet<>();
+		frutas01.add("maça");
+		frutas01.add("maça");
+		frutas01.add("banana");
+		frutas01.add("maça");
+		
+		for (String fruta : frutas01) {
+			System.out.println(fruta);
+			
+		}
+		
+		Set<String> frutas02 = new LinkedHashSet<>();
+		frutas02.add("maça");
+		frutas02.add("maça");
+		frutas02.add("banana");
+		frutas02.add("maça");
+		
+		for (String fruta : frutas02) {
+			System.out.println(fruta);
+			
+		}
+
+		
+		
 		
 		
 	}
